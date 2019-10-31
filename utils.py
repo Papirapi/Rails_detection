e4a46efb6594566677718459e7997abc2a323717
Badaoui Ahmed Badis <32328852+Papirapi@users.noreply.github.com>
2019-10-31 12:11:26 +0100

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,199 @@
+import csv
+import os
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+smooth = 1.
+def tversky_loss(y_true, y_pred):
+    alpha = 0.5
+    beta  = 0.5
+    
+    ones = K.ones(K.shape(y_true))
+    p0 = y_pred      # proba that voxels are class i
+    p1 = ones-y_pred # proba that voxels are not class i
+    g0 = y_true
+    g1 = ones-y_true
+    
+    num = K.sum(p0*g0, (0,1,2,3))
+    den = num + alpha*K.sum(p0*g1,(0,1,2,3)) + beta*K.sum(p1*g0,(0,1,2,3))
+    
+    T = K.sum(num/den) # when summing over classes, T has dynamic range [0 Ncl]
+    
+    Ncl = K.cast(K.shape(y_true)[-1], 'float32')
+    return Ncl-T
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f*y_true_f) + K.sum(y_pred_f*y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return 1.-dice_coef(y_true, y_pred)
+
+def _read_to_tensor(fname, output_height=224, output_width=244, normalize_data=False):
+    '''Function to read images from given image file path, and provide resized images as tensors
+        Inputs: 
+            fname - image file path
+            output_height - required output image height
+            output_width - required output image width
+            normalize_data - if True, normalize data to be centered around 0 (mean 0, range 0 to 1)
+        Output: Processed image tensors
+    '''
+    
+    # Read the image as a tensor
+    img_strings = tf.io.read_file(fname)
+    imgs_decoded = tf.image.decode_jpeg(img_strings)
+    
+    # Resize the image
+    output = tf.image.resize(imgs_decoded, [output_height, output_width])
+    
+    # Normalize if required
+    if normalize_data:
+        output = (output - 128) / 128
+    return output
+
+def read_images(img_dir,mask_dir):
+    '''Function to get all image directories, read images and masks in separate tensors
+        Inputs: 
+            img_dir - file directory
+        Outputs 
+            frame_tensors, masks_tensors, frame files list, mask files list
+    '''
+    
+            
+    # Separate frame and mask files lists, exclude unnecessary files
+    frames_list = [f for f in os.listdir(img_dir) if os.path.isfile(os.path.join(img_dir, f))]
+    masks_list = [f for f in os.listdir(mask_dir) if os.path.isfile(os.path.join(mask_dir, f))]
+    
+    frames_list.sort()
+    masks_list.sort()
+    
+    print('{} frame files found in the provided directory.'.format(len(frames_list)))
+    print('{} mask files found in the provided directory.'.format(len(masks_list)))
+    
+    # Create file paths from file names
+    frames_paths = [os.path.join(img_dir, fname) for fname in frames_list]
+    masks_paths = [os.path.join(mask_dir, fname) for fname in masks_list]
+    
+    # Create dataset of tensors
+    frame_data = tf.data.Dataset.from_tensor_slices(frames_paths)
+    masks_data = tf.data.Dataset.from_tensor_slices(masks_paths)
+    
+    # Read images into the tensor dataset
+    frame_tensors = frame_data.map(_read_to_tensor)
+    masks_tensors = masks_data.map(_read_to_tensor)
+    
+    print('Completed importing {} frame images from the provided directory.'.format(len(frames_list)))
+    print('Completed importing {} mask images from the provided directory.'.format(len(masks_list)))
+    
+    return frame_tensors, masks_tensors, frames_list, masks_list
+
+def parse_code(l):
+    '''Function to parse lines in a text file, returns separated elements (label codes and names in this case)
+    '''
+    if len(l.strip().split("\t")) == 2:
+        a, b = l.strip().split("\t")
+        return tuple(int(i) for i in a.split(' ')), b
+    else:
+        a, b, c = l.strip().split("\t")
+        return tuple(int(i) for i in a.split(' ')), c
+label_codes, label_names = zip(*[parse_code(l) for l in open('/valohai/inputs/label_colors/'+"label_colors.txt")])
+label_codes, label_names = list(label_codes), list(label_names)
+label_codes[:5], label_names[:5]
+
+print(label_codes, label_names)
+code2id = {v:k for k,v in enumerate(label_codes)}
+id2code = {k:v for k,v in enumerate(label_codes)}
+name2id = {v:k for k,v in enumerate(label_names)}
+id2name = {k:v for k,v in enumerate(label_names)}
+print(id2code,id2name)
+
+def rgb_to_onehot(rgb_image, colormap = id2code):
+    '''Function to one hot encode RGB mask labels
+        Inputs: 
+            rgb_image - image matrix (eg. 256 x 256 x 3 dimension numpy ndarray)
+            colormap - dictionary of color to label id
+        Output: One hot encoded image of dimensions (height x width x num_classes) where num_classes = len(colormap)
+    '''
+    num_classes = len(colormap)
+    shape = rgb_image.shape[:2]+(num_classes,)
+    encoded_image = np.zeros( shape, dtype=np.int8 )
+    for i, cls in enumerate(colormap):
+        encoded_image[:,:,i] = np.all(rgb_image.reshape( (-1,3) ) == colormap[i], axis=1).reshape(shape[:2])
+    return encoded_image
+
+def one_hot_it(label, label_values):
+    """
+    Convert a segmentation image label array to one-hot format
+    by replacing each pixel value with a vector of length num_classes
+
+    # Arguments
+        label: The 2D array segmentation image label
+        label_values
+        
+    # Returns
+        A 2D array with the same width and hieght as the input, but
+        with a depth size of num_classes
+    """
+   
+    semantic_map = []
+    for colour in label_values:
+        equality = np.equal(label, colour)
+        class_map = np.all(equality, axis = -1)
+        semantic_map.append(class_map)
+    semantic_map = np.stack(semantic_map, axis=-1)
+
+    return semantic_map
+    
+def reverse_one_hot(image):
+    """
+    Transform a 2D array in one-hot format (depth is num_classes),
+    to a 2D array with only 1 channel, where each pixel value is
+    the classified class key.
+
+    # Arguments
+        image: The one-hot format image 
+        
+    # Returns
+        A 2D array with the same width and hieght as the input, but
+        with a depth size of 1, where each pixel value is the classified 
+        class key.
+    """
+
+    x = np.argmax(image, axis = -1)
+    return x
+
+
+def colour_code_segmentation(image, label_values):
+    """
+    Given a 1-channel array of class keys, colour code the segmentation results.
+
+    # Arguments
+        image: single channel array where each value represents the class key.
+        label_values
+        
+    # Returns
+        Colour coded image for segmentation visualization
+    """
+
+    colour_codes = np.array(label_values)
+    x = colour_codes[image.astype(int)]
+
+    return x
+
+
+def get_label_values():
+    label_codes, label_names = zip(*[parse_code(l) for l in open('/valohai/inputs/label_colors/'+"label_colors.txt")])
+    label_values, class_names_list = list(label_codes), list(label_names)
+    class_names_string = ""
+    for class_name in class_names_list:
+        if not class_name == class_names_list[-1]:
+            class_names_string = class_names_string + class_name + ", "
+        else:
+            class_names_string = class_names_string + class_name
+
+    num_classes = len(label_values)
+
+    return label_values, class_names_list, num_classes
