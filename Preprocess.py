6acb6846b6468da2b3adcfee0e0956a3bcabba84
Badaoui Ahmed Badis <32328852+Papirapi@users.noreply.github.com>
2019-10-31 13:54:46 +0100

    Add files via upload

---
+++ Preprocess.py
@@ -0,0 +1,95 @@
+import numpy as np
+import os
+import random
+import re
+from PIL import Image
+import json
+from utils import *
+import sys
+import tensorflow as tf
+
+
+
+tf.compat.v1.enable_eager_execution()
+DATA_PATH = '/valohai/inputs/'
+
+def generate_image_folder_structure(frames, masks, frames_list, masks_list):
+    '''Function to save images in the appropriate folder directories 
+        Inputs: 
+            frames - frame tensor dataset
+            masks - mask tensor dataset
+            frames_list - frame file paths
+            masks_list - mask file paths
+    '''
+    #Create iterators for frames and masks
+    frame_batches = tf.compat.v1.data.make_one_shot_iterator(frames)  # outside of TF Eager, we would use make_one_shot_iterator
+    mask_batches = tf.compat.v1.data.make_one_shot_iterator(masks)
+    
+    #Iterate over the train images while saving the frames and masks in appropriate folders
+    dir_name='train'
+    for file in zip(frames_list[:-round(0.2*len(frames_list))],masks_list[:-round(0.2*len(masks_list))]):
+        
+        
+        #Convert tensors to numpy arrays
+        frame = frame_batches.next().numpy().astype(np.uint8)
+        mask = mask_batches.next().numpy().astype(np.uint8)
+        
+        #Convert numpy arrays to images
+        frame = Image.fromarray(frame)
+        mask = Image.fromarray(mask)
+        
+        #Save frames and masks to correct directories
+        frame.save(DATA_PATH+'{}_frames/{}'.format(dir_name,dir_name)+'/'+file[0])
+        mask.save(DATA_PATH+'{}_masks/{}'.format(dir_name,dir_name)+'/'+file[1])
+    
+    #Iterate over the val images while saving the frames and masks in appropriate folders
+    dir_name='val'
+    for file in zip(frames_list[-round(0.2*len(frames_list)):],masks_list[-round(0.2*len(masks_list)):]):
+        
+        
+        #Convert tensors to numpy arrays
+        frame = frame_batches.next().numpy().astype(np.uint8)
+        mask = mask_batches.next().numpy().astype(np.uint8)
+        
+        #Convert numpy arrays to images
+        frame = Image.fromarray(frame)
+        mask = Image.fromarray(mask)
+        
+        #Save frames and masks to correct directories
+        frame.save(DATA_PATH+'{}_frames/{}'.format(dir_name,dir_name)+'/'+file[0])
+        mask.save(DATA_PATH+'{}_masks/{}'.format(dir_name,dir_name)+'/'+file[1])
+    
+    print("Saved {} frames to directory {}".format(len(frames_list),DATA_PATH))
+    print("Saved {} masks to directory {}".format(len(masks_list),DATA_PATH))
+
+# img_dir = '/valohai/inputs/training-set-images/train_images/'
+# mask_dir = '/valohai/inputs/training-set-labels/train_image_segmentations/'
+# # Required image dimensions
+# output_height = 224
+# output_width = 224
+
+# frame_tensors, masks_tensors, frames_list, masks_list = read_images(img_dir,mask_dir)
+
+# # Make an iterator to extract images from the tensor dataset
+
+# frame_batches = tf.compat.v1.data.make_one_shot_iterator(frame_tensors)  # outside of TF Eager, we would use make_one_shot_iterator
+# mask_batches = tf.compat.v1.data.make_one_shot_iterator(masks_tensors)
+
+
+
+
+# # Create folders to hold images and masks
+
+# folders = ['train_frames/train', 'train_masks/train', 'val_frames/val', 'val_masks/val']
+
+
+# for folder in folders:
+#     try:
+#         os.makedirs(DATA_PATH + folder)
+#     except Exception as e: print(e)
+
+   
+# generate_image_folder_structure(frame_tensors, masks_tensors, frames_list, masks_list)
+
+# #generate_image_folder_structure(train_frames, train_masks, val_files, 'val')
+
