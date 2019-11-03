f8d250c88669b4e6158a3a513636d0781c334169
Badaoui Ahmed Badis <32328852+Papirapi@users.noreply.github.com>
2019-11-03 19:46:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 BATCH_SIZE = 5 #param
 EPOCHS = 10   #param
-PRETRAINED_MODEL_DIR = 'model_seg.h5'
-CHECKPOINT_SAVE= '/home/ubuntu/Desktop/learning/opencv-semantic-segmentation/videos/model_seg.h5'
+PRETRAINED_MODEL_DIR = 'my_model.h5'
 LEARNING_RATE = 0.001
 BATCH_SIZE = 5
 DEFAULT_SAVE_DIR = ''
